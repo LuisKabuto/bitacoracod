70e58ffd81ef1d472d3fc8e07420a33f8a6426a4
LuisKabuto <115038299+LuisKabuto@users.noreply.github.com>
2026-09-14 09:18:34 -0400

    Add v3 configuration module

---
+++ js/core/config.js
@@ -0,0 +1,7 @@
+/* Bitácora PRO v3 - configuración central */
+window.Bitacora = window.Bitacora || {};
+window.Bitacora.config = Object.freeze({
+  timezone: 'America/Caracas',
+  defaultSchedule: Object.freeze({ start: '08:00', end: '15:00' }),
+  lateToleranceMinutes: 10
+});
