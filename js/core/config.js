5d273f534301079fc91bdb42c73b78124d1742d3
LuisKabuto <115038299+LuisKabuto@users.noreply.github.com>
2026-09-21 14:58:31 -0400

    Add current temporary work schedule

--- js/core/config.js
+++ js/core/config.js
@@ -3,5 +3,6 @@ window.Bitacora = window.Bitacora || {};
 window.Bitacora.config = Object.freeze({
   timezone: 'America/Caracas',
   defaultSchedule: Object.freeze({ start: '08:00', end: '15:00' }),
+  currentSchedule: Object.freeze({ start: '08:00', end: '12:30' }),
   lateToleranceMinutes: 10
 });
